47aea9f57315ee2d1285c007845d8e3c425cdb3c
boykie <thefooser@gmail.com>
2023-11-06 00:14:04 +0800

    Added Database file 1.0

---
+++ GET DATABASE FILE HERE/bse_booking.sql
@@ -0,0 +1,266 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 05, 2023 at 05:09 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bse_booking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bidding_table`
+--
+
+CREATE TABLE `bidding_table` (
+  `id` int(11) NOT NULL,
+  `staff_id` varchar(255) NOT NULL,
+  `bidding_status` enum('approved','rejected','pending') NOT NULL DEFAULT 'pending',
+  `role` varchar(30) NOT NULL,
+  `slot_id` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bidding_table`
+--
+
+INSERT INTO `bidding_table` (`id`, `staff_id`, `bidding_status`, `role`, `slot_id`) VALUES
+(24, 'vandalim', 'pending', 'chef', '11/11/11'),
+(25, 'vandalim', 'approved', 'chef', '22/22/22'),
+(26, 'vandalim', 'rejected', 'chef', '33/33/33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cafe_staff`
+--
+
+CREATE TABLE `cafe_staff` (
+  `staffID` varchar(30) NOT NULL,
+  `staffRole` varchar(30) NOT NULL,
+  `userID` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cafe_staff`
+--
+
+INSERT INTO `cafe_staff` (`staffID`, `staffRole`, `userID`) VALUES
+('johndoe_chef', 'cashier', 'johndoe'),
+('rickkoe_chef', 'waiter', 'rickkoe'),
+('vandalim_chef', 'chef', 'vandalim');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_account`
+--
+
+CREATE TABLE `user_account` (
+  `userID` int(11) NOT NULL,
+  `userName` varchar(30) NOT NULL,
+  `userPassword` varchar(30) NOT NULL,
+  `userEmail` varchar(30) NOT NULL,
+  `userProfile` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_account`
+--
+
+INSERT INTO `user_account` (`userID`, `userName`, `userPassword`, `userEmail`, `userProfile`) VALUES
+(15, 'sysadmin', '1', 'asdf@gmail.com', 'admin'),
+(16, 'cafeowner', '1', 'lkjh@gmail.com', 'owner'),
+(17, 'cafemanager', '1', 'mnbv@gmail.com', 'manager'),
+(18, 'johndoe', '1', 'rewq@gmail.com', 'staff'),
+(19, 'rickkoe', '1', 'fhdt@gmail.com', 'staff'),
+(20, 'vandalim', '1', 'oibq@gmail.com', 'staff'),
+(25, 'cafeowner2', '1', 'asdz@gmail.com', 'owner');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_profile`
+--
+
+CREATE TABLE `user_profile` (
+  `userProfileType` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_profile`
+--
+
+INSERT INTO `user_profile` (`userProfileType`) VALUES
+('admin'),
+('manager'),
+('owner'),
+('staff');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `work_role`
+--
+
+CREATE TABLE `work_role` (
+  `role` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `work_role`
+--
+
+INSERT INTO `work_role` (`role`) VALUES
+('cashier'),
+('chef'),
+('waiter');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `work_slot`
+--
+
+CREATE TABLE `work_slot` (
+  `slotID` int(11) NOT NULL,
+  `ownerID` varchar(30) NOT NULL,
+  `chefSlot` int(11) NOT NULL,
+  `cashierSlot` int(11) NOT NULL,
+  `waiterSlot` int(11) NOT NULL,
+  `slotDate` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `work_slot`
+--
+
+INSERT INTO `work_slot` (`slotID`, `ownerID`, `chefSlot`, `cashierSlot`, `waiterSlot`, `slotDate`) VALUES
+(1, 'cafeowner', 0, 0, 0, '11/11/11'),
+(2, 'cafeowner', 0, 0, 0, '22/22/22'),
+(3, 'cafeowner', 0, 0, 0, '33/33/33'),
+(4, 'cafeowner2', 0, 0, 0, '88/88/88'),
+(5, 'cafeowner2', 0, 0, 0, '99/99/99'),
+(7, 'cafeowner2', 0, 0, 0, '10/10/10');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bidding_table`
+--
+ALTER TABLE `bidding_table`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `slot_id` (`slot_id`),
+  ADD KEY `staff_id` (`staff_id`),
+  ADD KEY `role` (`role`);
+
+--
+-- Indexes for table `cafe_staff`
+--
+ALTER TABLE `cafe_staff`
+  ADD PRIMARY KEY (`staffID`),
+  ADD KEY `staffRole` (`staffRole`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `user_account`
+--
+ALTER TABLE `user_account`
+  ADD PRIMARY KEY (`userID`),
+  ADD UNIQUE KEY `USER_ACCOUNT_CKEY` (`userName`),
+  ADD KEY `userProfile` (`userProfile`);
+
+--
+-- Indexes for table `user_profile`
+--
+ALTER TABLE `user_profile`
+  ADD PRIMARY KEY (`userProfileType`);
+
+--
+-- Indexes for table `work_role`
+--
+ALTER TABLE `work_role`
+  ADD PRIMARY KEY (`role`);
+
+--
+-- Indexes for table `work_slot`
+--
+ALTER TABLE `work_slot`
+  ADD PRIMARY KEY (`slotID`),
+  ADD UNIQUE KEY `slotDate` (`slotDate`),
+  ADD KEY `ownerID` (`ownerID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bidding_table`
+--
+ALTER TABLE `bidding_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `user_account`
+--
+ALTER TABLE `user_account`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `work_slot`
+--
+ALTER TABLE `work_slot`
+  MODIFY `slotID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bidding_table`
+--
+ALTER TABLE `bidding_table`
+  ADD CONSTRAINT `bidding_table_ibfk_1` FOREIGN KEY (`slot_id`) REFERENCES `work_slot` (`slotDate`),
+  ADD CONSTRAINT `bidding_table_ibfk_2` FOREIGN KEY (`staff_id`) REFERENCES `user_account` (`userName`),
+  ADD CONSTRAINT `bidding_table_ibfk_3` FOREIGN KEY (`role`) REFERENCES `work_role` (`role`);
+
+--
+-- Constraints for table `cafe_staff`
+--
+ALTER TABLE `cafe_staff`
+  ADD CONSTRAINT `cafe_staff_ibfk_1` FOREIGN KEY (`staffRole`) REFERENCES `work_role` (`role`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `cafe_staff_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `user_account` (`userName`);
+
+--
+-- Constraints for table `user_account`
+--
+ALTER TABLE `user_account`
+  ADD CONSTRAINT `user_account_ibfk_1` FOREIGN KEY (`userProfile`) REFERENCES `user_profile` (`userProfileType`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `work_slot`
+--
+ALTER TABLE `work_slot`
+  ADD CONSTRAINT `work_slot_ibfk_1` FOREIGN KEY (`ownerID`) REFERENCES `user_account` (`userName`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
